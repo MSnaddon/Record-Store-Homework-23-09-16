8b8f6fd2728f3e430179b1a147ba6ab4c77df3f3
Michael Snaddon <msnaddon0991@gmail.com>
2016-09-23 11:17:41 +0100

    More tests added

---
+++ record_store.js
@@ -0,0 +1,7 @@
+var RecordStore = function(name, city){
+  this.name = name;
+  this.city = city;
+  this.cash = 1500;
+}
+
+module.exports = RecordStore;
--- specs/record_store_specs.js
+++ specs/record_store_specs.js
@@ -1,5 +1,5 @@
 var assert = require("assert");
-var recordStore = require("recordStore");
+var RecordStore = require("../record_store");
 
 
 describe("Record store", function(){
@@ -7,11 +7,16 @@ describe("Record store", function(){
   var recordStore;
 
   beforeEach(function(){
-
+    recordStore = new RecordStore("Underwave models ltd", "Catwalk");
   })
 
   it("should have a name and city", function(){
+    assert.equal(recordStore.name, "Underwave models ltd")
+    assert.equal(recordStore.city, "Catwalk")
+  })
 
+  it("should have a property of how much cash is in the shop", function(){
+    assert.equal(recordStore.cash, 1500);
   })
 
 })
