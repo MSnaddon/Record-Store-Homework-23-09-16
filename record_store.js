039c850f072ebd8f06639c487642773d5675a8ee
Michael Snaddon <msnaddon0991@gmail.com>
2016-09-23 11:38:53 +0100

    stock array added

--- record_store.js
+++ record_store.js
@@ -2,6 +2,9 @@ var RecordStore = function(name, city){
   this.name = name;
   this.city = city;
   this.cash = 1500;
+  this.stock = [];
 }
+
+
 
 module.exports = RecordStore;
--- specs/record_store_specs.js
+++ specs/record_store_specs.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var RecordStore = require("../record_store");
+var Record = require("../record")
 
 
 describe("Record store", function(){
@@ -7,6 +8,9 @@ describe("Record store", function(){
   var recordStore;
 
   beforeEach(function(){
+    record1 = new Record("Paul", "The underwear and the undertear", 5.99)
+    record2 = new Record("Patrisha", "Loungery", 13.99)
+    record3 = new Record("The Backstrap Boys", "Function and form", 9.99)
     recordStore = new RecordStore("Underwave models ltd", "Catwalk");
   })
 
@@ -17,6 +21,15 @@ describe("Record store", function(){
 
   it("should have a property of how much cash is in the shop", function(){
     assert.equal(recordStore.cash, 1500);
+  })
+
+  it("somewhere to put records", function(){
+    assert.deepEqual([], recordStore.stock)
+  })
+
+  it("can add stock to inventory", function(){
+    recordStore.addStock([record1, record2, record3]);
+    assert.equal(3, recordStore.stock);
   })
 
 })
